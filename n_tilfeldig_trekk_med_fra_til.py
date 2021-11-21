af8d33b0c2638f0e629fe134b463e88dc36b71bb
TorGj <82477510+TorGj@users.noreply.github.com>
2021-11-21 17:02:10 +0100

    Test of random function
    User set start number and end number.
    Code does a number (default 10 000) random pick between users input, and then counts how many of each was done. Result is written to file in two ways and formats.

---
+++ n_tilfeldig_trekk_med_fra_til.py
@@ -0,0 +1,49 @@
+import random     # Random ting
+import json       # Eksporterings ting
+# import time
+# import numpy
+
+r = 0
+#navn = input('Navn: ')
+n = int(input('Fra nr! '))
+m = 1+int(input('Til nr! '))
+
+
+p=[0]*(m-n)    #Hver posisjon settes til verdien 0, og lengden settes
+
+def lage():
+    tilfeldig = random.randrange(n,m,1) # trekk: (fra,til,trinnstørrelse)
+    #print(tilfeldig)
+    t = m-(tilfeldig+1)
+    p[t] = p[t]+1         # her legges 1 til antall utfall av verdien 
+    return tilfeldig
+
+ 
+while not (r==10000):   # Her bestemmes hvor mange trekk som skal gjøres
+    lage()
+    r = r+1
+    
+print(p)
+
+
+
+
+def skriv_til_fil():
+    fil_nr_1 = open('myfile.txt', 'w') # åpner for skriving til fil..
+    global m
+    c = 0
+    #i = 0
+    for i in p:
+        print('Antall ', m-1 ,'-tall er: ', p[c])
+        fil_nr_1.write('Antall ' + str(m-1) + '-tall er: ' + str(p[c])+ '\n')
+        c=c+1
+        
+        m = m-1
+    fil_nr_1.close()
+    
+
+skriv_til_fil()
+
+# Skriver til en fil til, men med et mer standard format...
+with open('myfile2.txt', 'w') as filehandle:
+    json.dump(p, filehandle)
